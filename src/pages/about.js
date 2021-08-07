53ab8ff004ed30ef772c202ab2a9e2f055afaa44
GaryBarnes13 <gary.barnesg@gmail.com>
2021-08-07 09:43:47 -0400

    Finished Part 2 of Tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function about() {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I am the proud creator of this site, which I built with
+        Gatsby.
+      </p>
+    </Layout>
+  );
+}
